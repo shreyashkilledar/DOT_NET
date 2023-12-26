b855445fecf0992ad67bc10761ad7d0b7f4165fd
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2023-12-26 03:39:09 -0900

    Add files via upload

---
+++ DOTNET_45/TestApp/CSSolution/CSFeatureApp/Maths.cs
@@ -0,0 +1,17 @@
+namespace Mathematics;
+
+public sealed class Mathematics1
+{
+        public int Addition(int a1, int a2)
+        {
+            int a3=a1+a2;
+            return a3;
+        }
+
+        public int Substraction(int a3, int a4)
+        {
+            int a5=a3-a4;
+            return a5;
+        }
+
+}
