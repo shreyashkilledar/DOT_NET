dd7d2f6bd85bf2c70e9b152c6dbab2dd8b486f0f
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2023-12-27 08:48:26 -0900

    Add files via upload

---
+++ WebSolution_Demo/BOL/bin/Debug/net8.0/Product.cs
@@ -0,0 +1,79 @@
+﻿namespace BOL;
+
+public class Product
+{
+
+    public int Id{
+       get;
+       set;
+    }
+    public string Name{
+        get;
+        set;
+    }
+    public string Description
+    {
+        get;
+        set;
+    }
+    public int Qty{
+        get;
+        set;
+    }
+    public int Price
+    {
+        get;
+        set;
+    }
+
+    // private int id;
+    // private string name;
+    // private string description;
+    // private int qty;
+    // private int price;
+
+    // public Product(int id,string name,string description,int qty,int price)
+    // {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.description = description;
+    //     this.qty = qty;
+    //     this.price = price;
+    // }
+    
+//     //Properties of Product Entity
+
+//     public int id
+//     {
+//         get { return id; }
+//         set { id = value; }
+//     }
+
+//     public int name
+//     {
+//         get { return name; }
+//         set { name = value; }
+//     }
+      
+//     public int description
+//     {
+//         get { return description; }
+//         set { description = value; }
+//     }
+   
+//     public int qty
+//     {
+//         get { return qty; }
+//         set { qty = value; }
+//     }
+
+//    public int price
+//     {
+//         get { return price; }
+//         set { price = value; }
+//     }
+    
+    
+
+
+}
