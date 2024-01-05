d1cca6345f3b20b8fa4d3574c20a05f0182a7bf5
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2024-01-05 09:06:04 +0530

    Add files via upload

---
+++ StudentApp copy/DBFile.sql
@@ -0,0 +1,3 @@
+create table Students(Id int, Fname varchar(20),Lname varchar (20), Standard int);
+insert into students values(1,"Shubham","Lokhande",10);
+insert into students values(2,"Omkar","Lokhande",12);
